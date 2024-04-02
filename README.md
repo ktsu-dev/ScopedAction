75eb7def0d8ed0101f96ae62f55248980aabecde
matt-edmondson <matthew.edmondson@gmail.com>
2024-04-03 08:47:56 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ScopedAction
+Cleanly call actions at the start and end of a scope block.
---
+++ ScopedAction/ScopedAction.cs
@@ -0,0 +1,41 @@
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace ktsu.io.ScopedAction;
+
+public class ScopedAction : IDisposable
+{
+	private bool disposedValue;
+	private Action? OnClose { get; }
+	public ScopedAction(Action? onOpen, Action? onClose)
+	{
+		OnClose = onClose;
+		onOpen?.Invoke();
+	}
+
+	/// <summary>
+	/// Dispose of the <see cref="ScopedAction"/>.
+	/// </summary>
+	/// <param name="disposing"></param>
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
+			{
+				OnClose?.Invoke();
+			}
+
+			disposedValue = true;
+		}
+	}
+
+	/// <summary>
+	/// Dispose of the <see cref="ScopedAction"/>.
+	/// </summary>
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
