acf44f19e90ec9a82bc9a0b1935492dae5b90185
Matt Edmondson <matthew.edmondson@gmail.com>
2024-09-14 15:35:58 +1000

    Migrate ktsu.io to ktsu namespace

--- ScopedAction/ScopedAction.cs
+++ ScopedAction/ScopedAction.cs
@@ -1,6 +1,6 @@
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-namespace ktsu.io.ScopedAction;
+namespace ktsu.ScopedAction;
 
 public class ScopedAction : IDisposable
 {
